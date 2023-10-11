e759ecd21b156d9fc034db99873c15494859ac58
unknown <evylyn-word@hotmail.com>
2023-10-11 10:04:09 -0300

    ong_ficar_bem

--- setup/urls.py
+++ setup/urls.py
@@ -1,3 +1,5 @@
+"""
+URL configuration for setup project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from galeria.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+        path('', index, name='index'), 
 ]
