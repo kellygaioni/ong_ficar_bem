b2f6ff9e3e77f338bd122cc62af0c54880bc6a2e
unknown <evylyn-word@hotmail.com>
2023-10-11 09:43:56 -0300

    ong_ficar_bem

--- setup/urls.py
+++ setup/urls.py
@@ -1,5 +1,3 @@
-"""
-URL configuration for setup project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
